5d1d9a8973e65308124125a0c54ce9d975b897a6
Asiatikunik <dutunikdang@gmail.com>
2014-10-19 23:57:20 +0700

    Savegard en cas de crash

--- game.cc
+++ game.cc
@@ -7,15 +7,16 @@
 #include <fcntl.h>
 using namespace std;
 
-/*Asian Situation Test maison */
-
-/* retourne 0 si aucune touche n'est appuyée, 
- * sinon retourne le code ASCII de la touche */
 int read_keybord();
 
 /******************************************************************************/
-#define H 25 
-#define L 50
+#define H 30 
+#define L 60
+int barreD= (L/2) -5;
+int barreF= (L/2) +5;
+double VitesseBalle=1;
+int VitesseJeu=100;
+int VitesseBarre=3;
 
 char screen[H][L];
 
@@ -23,11 +24,11 @@ void screen_display() {
 	system("clear");
 	for (int i=0; i<H; i++) {
 		for (int j=0; j<L; j++) {
-			
+
 			if(screen[i][j]==1)
-				printf("\033[36;01mX\033[00m");
+				printf("\033[36;01mO\033[00m");
 			else
-			cout << screen[i][j];
+				cout << screen[i][j];
 		}
 		cout << endl;
 	}
@@ -36,65 +37,104 @@ void screen_display() {
 void clear_screen() {
 	for (int i=0; i<H; i++) {
 		for (int j=0; j<L; j++) {
+			//Le screen
 			if(i==0 || i== H-1 || j==0 || j==L-1) {
-				screen[i][j] = '*';
+				screen[i][j] = '-';
+				if(j==0 || j==L-1) 
+					screen[i][j] = '|';
 			}else{
-			screen[i][j] = ' ';
+				screen[i][j] = ' ';
+			}
+
+			//La barre
+			if (i==H-3 && j> barreD && j< barreF ) { 
+				screen[i][j]='o';
 			}
 		}
 	}
 }
 
-/******************************************************************************/
-double x = 2;
+double x=2;
 double y = 5;
 bool vertical;
 bool horizontal;
 
 void update_game(int key) {
-	double vert;
-	double hori;
 
 	screen[(int)x][(int)y] = 1;
 
-if (key=='s') 
-	vertical=0;
 
-if (key == 'z') 
-	vertical=1;
+	//Déplacement de la barre
+	if(key=='s') {
+		if(barreD>=1) {
+			barreD=barreD-VitesseBarre;
+			barreF=barreF-VitesseBarre;
+		}	
+	}
 
-if(key== 'q')
-	horizontal=1;
+	if(key=='d') {
+		if(barreF<L-1) {	
+			barreD=barreD+VitesseBarre;
+			barreF=barreF+VitesseBarre;
+		}
+	}
 
-if(key== 'd')
-	horizontal=0;
 
-if ( vertical==0 ) {
-	x = x + 1;
-	if(x==25)
+	//Le rebond de la barre
+
+	if( y>barreD && y<barreF && x==H-3) {
 		vertical=1;
 	}
 
-if (horizontal==0 ) {
-	y = y + 1;
-	if(y==50)
-		horizontal=1;
+
+	//Limite du screen
+	if ( vertical==0 ) {
+		x = x + VitesseBalle;
+		cout << "bas " << x << endl;
+		if(x==H)
+			vertical=1;
+	} /*else{
+		x = x - VitesseBalle;
+		cout << "haut " << x <<endl;
+		if(x==0)
+			vertical=0;
+
+	} */
+
+	if (horizontal==0 ) {
+		y = y + VitesseBalle;
+		cout << "droit " << y <<endl;
+		if(y==L)
+			horizontal=1;
+	} /*else{
+		y=y-VitesseBalle;
+		cout <<"gauche " << y << endl;
+		if(y==0)
+			horizontal=0;
+
+	}*/
+
+	if(horizontal==1) {
+		y=y-VitesseBalle;
+		if(y==0)	
+			horizontal=0;
 	}
 
-if (vertical==1) {
-	x = x - 1;
-	if(x==0	)
-		vertical=0;
+	if (vertical==1) {
+		x = x - VitesseBalle;
+		if(x==0)
+			vertical=0;
 	}
 
-if (horizontal==1 ) {
-	y = y - 1;
-	if(y==0) 
+	/*	if (horizontal==1 ) {
+		y = y - VitesseBalle;
+		if(y==0) 
 		horizontal=0;
+		}	
+
+	 */
 }
 
-
-}
 
 /******************************************************************************/
 
@@ -105,14 +145,13 @@ int main() {
 		clear_screen();
 		update_game(key);
 		screen_display();
-		usleep(100 * 1000);
+		usleep(VitesseJeu * 1000);
 	} while (key != 'y');
 }
 
 /******************************************************************************/
 
-int kbhit(void)
-{
+int kbhit(void) {
 	struct termios oldt, newt;
 	int ch;
 	int oldf;
@@ -129,8 +168,7 @@ int kbhit(void)
 	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 	fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-	if(ch != EOF)
-	{
+	if(ch != EOF){
 		ungetc(ch, stdin);
 		return 1;
 	}
@@ -149,4 +187,3 @@ int read_keybord() {
 
 /******************************************************************************/
 /******************************************************************************/
-
